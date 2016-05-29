64fbb9ac4371377a60157a4bbcd6f0e4f51b75ce
alakmo <alakmo2004@yahoo.com>
2016-05-29 17:46:16 -0400

    Update and rename QuestionA.java to O(n+m)_time_and_spaceQuestionA.java

--- Ch 04. Trees and Graphs/Q4_10_Check_Subtree/O(n+m)_time_and_spaceQuestionA.java
+++ Ch 04. Trees and Graphs/Q4_10_Check_Subtree/O(n+m)_time_and_spaceQuestionA.java
@@ -52,3 +52,26 @@ public class QuestionA {
 	}
 
 }
+/**
+ * import java.io.*;
+
+public class Test {
+
+   public static void main(String args[]) {
+      String Str = new String("Welcome to Tutorialspoint.com");
+      String SubStr1 = new String("Tutorials");
+      String SubStr2 = new String("Sutorials");
+
+      System.out.print("Found Index :" );
+      System.out.println(Str.indexOf( 'o' ));
+      System.out.print("Found Index :" );
+      System.out.println(Str.indexOf( 'o', 5 ));
+      System.out.print("Found Index :" );
+      System.out.println( Str.indexOf( SubStr1 ));
+      System.out.print("Found Index :" );
+      System.out.println( Str.indexOf( SubStr1, 15 ));
+      System.out.print("Found Index :" );
+      System.out.println(Str.indexOf( SubStr2 ));
+   }
+}
+ * **/
