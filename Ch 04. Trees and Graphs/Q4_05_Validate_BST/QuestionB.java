a8561bdb0752e90e4b4a3fc3bc4d3260b3997aee
alakmo <alakmo2004@yahoo.com>
2016-05-29 16:26:44 -0400

    Update QuestionB.java

--- Ch 04. Trees and Graphs/Q4_05_Validate_BST/QuestionB.java
+++ Ch 04. Trees and Graphs/Q4_05_Validate_BST/QuestionB.java
@@ -2,7 +2,7 @@ package Q4_05_Validate_BST;
 
 import CtCILibrary.AssortedMethods;
 import CtCILibrary.TreeNode;
-
+//O(n) time  O(logn) space
 public class QuestionB {
 	public static boolean checkBST(TreeNode n, Integer min, Integer max) {
 		if (n == null) {
@@ -18,68 +18,15 @@ public class QuestionB {
 		return true;
 	}
 		
+		
+		
+		
 	public static boolean checkBST(TreeNode n) {
 		return checkBST(n, null, null);
 	}
 	
-	public static boolean checkBSTAlternate(TreeNode n) {
-		return checkBSTAlternate(n, new IntWrapper(0), new IntWrapper(0));
-	}		
 
-	public static boolean checkBSTAlternate(TreeNode n, IntWrapper min, IntWrapper max) {
-		/* An alternate, less clean approach. This is not provided in the book, but is used to test the other method. */
-		if (n.left == null) {
-			min.data = n.data;
-		} else {
-			IntWrapper leftMin = new IntWrapper(0);
-			IntWrapper leftMax = new IntWrapper(0);
-			if (!checkBSTAlternate(n.left, leftMin, leftMax)) {
-				return false;
-			}
-			if (leftMax.data > n.data) {
-				return false;
-			}
-			min.data = leftMin.data;
-		}
-		if (n.right == null) {
-			max.data = n.data;
-		} else {
-			IntWrapper rightMin = new IntWrapper(0);
-			IntWrapper rightMax = new IntWrapper(0);
-			if (!checkBSTAlternate(n.right, rightMin, rightMax)) {
-				return false;
-			}
-			if (rightMin.data <= n.data) {
-				return false;
-			}
-			max.data = rightMax.data;
-		}
-		return true;
-	}
 
-	/* Create a tree that may or may not be a BST */
-	public static TreeNode createTestTree() {
-		/* Create a random BST */
-		TreeNode head = AssortedMethods.randomBST(10, -10, 10); 
-		
-		/* Insert an element into the BST and potentially ruin the BST property */
-		TreeNode node = head;
-		do {
-			int n = AssortedMethods.randomIntInRange(-10, 10);
-			int rand = AssortedMethods.randomIntInRange(0, 5);
-			if (rand == 0) {
-				node.data = n;
-			} else if (rand == 1) {
-				node = node.left;
-			} else if (rand == 2) {
-				node = node.right;
-			} else if (rand == 3 || rand == 4) {
-				break;
-			}
-		} while (node != null);	
-		
-		return head;
-	}
 	
 	public static void main(String[] args) {
 		/* Simple test -- create one */
