2509aed44fd5fc6e477bbe8fdae1126a8cad106b
alakmo <alakmo2004@yahoo.com>
2016-05-29 20:52:57 -0400

    Update Question.java

--- Ch 05. Bit Manipulation/Q5_06_Conversion/Question.java
+++ Ch 05. Bit Manipulation/Q5_06_Conversion/Question.java
@@ -3,14 +3,14 @@ package Q5_06_Conversion;
 import CtCILibrary.AssortedMethods;
 
 public class Question {
-	public static int bitSwapRequired(int a, int b) {
+/***	public static int bitSwapRequired(int a, int b) {
 		int count = 0;
 		for (int c = a ^ b; c != 0; c = c >> 1) { 
 			count += c & 1;
 		}
 		return count;
 	}
-	
+**/
 	public static int bitSwapRequired2(int a, int b){
 		int count = 0;
 		for (int c = a ^ b; c != 0; c = c & (c-1)) {
