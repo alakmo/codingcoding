e1769d22013b7add48bc1a15628a185068e3c5b9
alakmo <alakmo2004@yahoo.com>
2016-05-30 17:59:04 -0400

    Create stringToBool.java

---
+++ stringToBool.java
@@ -0,0 +1,17 @@
+	public static int count = 0;
+	public static boolean stringToBool(String c) {
+		return c.equals("1") ? true : false;
+	}
+	
+	public static int countEval(String s, boolean result) {
+		count++;
+		if (s.length() == 0) return 0;
+		if (s.length() == 1) return stringToBool(s) == result ? 1 : 0;
+
+		int ways = 0;
+		
+		for (int i = 1; i < s.length(); i += 2) {
+			char c = s.charAt(i);
+			String left = s.substring(0, i);
+			String right = s.substring(i + 1, s.length());
+			
