74f6f3cca65b46749cc5f8255101895c7258845a
alakmo <alakmo2004@yahoo.com>
2016-06-01 00:35:26 -0400

    Update Stream_Matrix.java

--- Stream_Matrix.java
+++ Stream_Matrix.java
@@ -15,3 +15,29 @@
 		    e.printStackTrace();            
 		}   
 		
+
+
+
+
+
+			 int c=0;
+			ArrayList<String> list=new ArrayList<String>();
+			
+			try(Stream<String> stream = Files.lines(Paths.get("input1.txt"))){
+				list= (ArrayList<String>) stream.map(line -> line.trim())
+		          .filter(line -> !line.isEmpty()).collect(Collectors.toList());
+		        		  
+		          
+		          //forEach(line->list.add(Integer.parseInt(line)));
+			}catch(Exception e){    
+			    e.printStackTrace();            
+			}  
+			//list = [44 1 2, 111, 010, 1050, 100]
+			
+			
+			
+			
+			
+			
+			
+			
